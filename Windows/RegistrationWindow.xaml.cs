8e945878158a284060c32523b163d870f6a55bf8
Blackays <43548329+Blackays@users.noreply.github.com>
2019-11-04 12:07:22 +0200

    Registration(part 1)

--- Windows/RegistrationWindow.xaml.cs
+++ Windows/RegistrationWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +26,22 @@ namespace AEGEE_Project.Windows
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Users (Name, Surname, Login, Password) VALUES(@name, @surname, @login, @password)";
+            cmd.Parameters.AddWithValue("@name", NameBox.Text);
+            cmd.Parameters.AddWithValue("@surname", SurnameBox.Text);
+            cmd.Parameters.AddWithValue("@login", LoginBox.Text);
+            cmd.Parameters.AddWithValue("@password", PasswordBox.Text);
+            cmd.Connection = con;
+            int a = cmd.ExecuteNonQuery();
+            if(a==1)
+            {
+                MessageBox.Show("Added");
+            }
+            else { MessageBox.Show("Not Added"); }
 
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
